87362f6a02518d7eab0cda5875b48c60866ae151
Harry Stead <harrystead9@gmail.com>
2021-03-30 10:02:09 +0100

    finished global context, added to transcation list, added a transaction component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from "react";
+import Transaction from './Transaction';
+import { GlobalContext } from "../context/GlobalState";
 
 const TransactionList = () => {
-    return (
-        <>
-        <h3>History</h3>
-        <ul className="list">
-        </ul>
-      </>
-    );
+  const { transactions } = useContext(GlobalContext);
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction}/>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default TransactionList;
---
+++ src/context/GlobalState.js
@@ -0,0 +1,25 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from './AppReducer';
+
+const initialState = {
+    transactions: [
+        {id: 1, text: "Flowers", amount: -20},
+        {id: 2, text: "Salary", amount: 400},
+        {id: 3, text: "Books", amount: -10},
+        {id: 4, text: "Camera", amount: -150},
+    ]
+}
+
+//global
+export const GlobalContext = createContext(initialState);
+
+//provider 
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}>
+    {children}
+    </GlobalContext.Provider>)
+}
